e0c087e185b622bcc51820dcefa6e2cddbcf79e5
saroar45 <saroarjahans@gmail.com>
2022-11-10 15:48:23 +0600

    working on examples

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,6 +1,16 @@
+#include <DS3231M.h>
+#include <Wire.h>
+#include <EEPROM.h>
+//init DS3231M
+DS3231  rtc(SDA, SCL);
+// Init a Time-data structure
+Time t;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  rtc.begin();
+  t = rtc.getTime();
+  Serial.println(t.year,DEC);
 }
 
 void loop() {
